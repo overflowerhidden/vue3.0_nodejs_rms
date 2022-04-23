6c07dc54c6d28de356deadd611981d760b60e01d
pengpeng <1064155364@qq.com>
2022-04-23 16:37:14 +0800

    storage 二次封装

--- src/App.vue
+++ src/App.vue
@@ -5,6 +5,12 @@
 <script>
 export default {
   name: "app",
+  mounted() {
+    this.$storage.setItem("info", { name: "zhaopeng", age: 27 });
+    // console.log("info=>", this.$storage.getItem("info"));
+    // this.$storage.clearItem("age");
+    this.$storage.clearAll();
+  },
 };
 </script>
 
--- src/main.js
+++ src/main.js
@@ -4,10 +4,12 @@ import 'element-plus/dist/index.css'
 import App from './App.vue'
 import router from './router'
 import request from './utils/request'
+import storage from './utils/storage'
 
 console.log('环境变量：', import.meta.env);
 
 const app = createApp(App);
 // 添加全局
 app.config.globalProperties.$request = request;
+app.config.globalProperties.$storage = storage;
 app.use(router).use(ElementPlus).mount('#app');
